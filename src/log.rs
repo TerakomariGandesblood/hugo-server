96b617e8bfe1a05e7fdd2a914a18710e9dcb4669
Terakomari <im.terakomari@icloud.com>
2026-03-24 15:55:57 +0800

    feat: use utc time in log

--- src/log.rs
+++ src/log.rs
@@ -1,14 +1,16 @@
 use std::path::Path;
-use std::{env, io};
+use std::{env, fs, io};
 
 use anyhow::Result;
 use clap_verbosity_flag::Verbosity;
-use logroller::{LogRollerBuilder, Rotation, RotationAge, TimeZone};
+use jiff::Timestamp;
 use supports_color::Stream;
 use tracing_appender::non_blocking::WorkerGuard;
+use tracing_appender::rolling::{RollingFileAppender, Rotation};
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::fmt::Layer;
-use tracing_subscriber::fmt::time::ChronoLocal;
+use tracing_subscriber::fmt::format::Writer;
+use tracing_subscriber::fmt::time::FormatTime;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
@@ -37,22 +39,25 @@ where
         .expect("the file name is not in valid UTF-8")
         .to_string();
 
-    let file_appender = LogRollerBuilder::new(log_directory.as_ref(), Path::new(&log_file_name))
-        .rotation(Rotation::AgeBased(RotationAge::Daily))
-        .time_zone(TimeZone::Local)
-        .max_keep_files(7)
-        .graceful_shutdown(true)
-        .build()?;
+    if !log_directory.as_ref().try_exists()? {
+        fs::create_dir_all(&log_directory)?;
+    }
+
+    let file_appender = RollingFileAppender::builder()
+        .rotation(Rotation::DAILY)
+        .filename_prefix(log_file_name)
+        .max_log_files(7)
+        .build(log_directory)?;
 
     let (file_writer, guard) = tracing_appender::non_blocking(file_appender);
     let file_layer = Layer::new()
         .with_writer(file_writer)
-        .with_timer(ChronoLocal::rfc_3339())
+        .with_timer(JiffTimer)
         .with_ansi(false);
 
     let stdout_layer = Layer::new()
         .with_writer(io::stdout)
-        .with_timer(ChronoLocal::rfc_3339())
+        .with_timer(JiffTimer)
         .with_ansi(supports_color::on(Stream::Stdout).is_some());
 
     tracing_subscriber::registry()
@@ -62,6 +67,14 @@ where
         .init();
 
     Ok(guard)
+}
+
+struct JiffTimer;
+
+impl FormatTime for JiffTimer {
+    fn format_time(&self, w: &mut Writer<'_>) -> std::fmt::Result {
+        write!(w, "{}", Timestamp::now())
+    }
 }
 
 #[cfg(test)]
